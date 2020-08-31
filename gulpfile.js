a8608f8d8b49004b11241f79dd4bda96804bfde6
dp <new666@qq.com>
2020-08-31 11:43:01 +0800

    sass文件配置与头尾结构样式编写

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var { src , dest , series , parallel , watch } = require('gulp');
 var clean = require('gulp-clean');
 var fileInclude = require('gulp-file-include');
 var webserver = require('gulp-webserver');
+var sass = require('gulp-sass');
 
 function cleanTask(){
     return src('./dist' , {allowEmpty : true})
@@ -19,23 +20,36 @@ function fileIncludeTask(){
 }
 
 function webserverTask(){
-    return src('./dist/view')      //是view文件夹下作为localhost根目录
+    return src('./dist')      //是view文件夹下作为localhost根目录
             .pipe( webserver({
                 host : 'localhost',
                 port : 4000,
-                open : './index.html',
+                open : './view/index.html',
                 livereload : true 
             }));
 }
 
+function sassTask(){
+    return src('./src/css/*.scss')
+            .pipe(sass())
+            .pipe(dest('./dist/css'));
+}
+
+function staticTask(){   //把src中的静态资源同步到dist下
+    return src('./src/static/**')
+            .pipe(dest('./dist/static'));
+}
+
 function watchTask(){   //监听文件变化，同步到dist文件下
     watch('./src/view/**' , fileIncludeTask);
+    watch('./src/css/**' , sassTask);
+    watch('./src/static/**' , staticTask);
 }
 
 
 module.exports = {
     // 开发环境下的命令
-    dev : series( cleanTask , fileIncludeTask , parallel(webserverTask , watchTask) ),    
+    dev : series( cleanTask , parallel(fileIncludeTask , sassTask , staticTask) , parallel(webserverTask , watchTask) ),    
     // 生产环境下的命令
     build : series( cleanTask )
 };
